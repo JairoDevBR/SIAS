deb17a963c86e02cdb0346f7a53d3606cd100cd8
Rebeca Yun <rebecajhyun@gmail.com>
2024-03-14 18:16:24 -0300

    contagem de caracteres

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,4 +5,7 @@ class Schedule < ApplicationRecord
   belongs_to :worker2, class_name: 'Worker'
   belongs_to :user
   has_many :emergencies
+
+  validates :worker1_id, presence: true, numericality: { greater_than: 0 }
+  validates :worker2_id, presence: true, numericality: { greater_than: 0 }
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,4 +1,7 @@
 class Worker < ApplicationRecord
   has_many :first_schedules, class_name: 'Schedule', foreign_key: 'worker1_id'
   has_many :second_schedules, class_name: 'Schedule', foreign_key: 'worker2_id'
+
+  validates :name, presence: true
+  validates :occupation, presence: true
 end
