a85394599705602700ef931ac8ae78078b4c6936
Keny Ng <kcln88@hotmail.com>
2024-03-17 18:54:17 -0300

    ao schedule logar, todas as emergencias nao encerradas que ele tinha, serao atribuidas schedule_id=nil

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -33,8 +33,14 @@ class SchedulesController < ApplicationController
     @schedule.active = true
     authorize @schedule
     # logica para atribuir active = false para todas as schedules do mesmo user, com excecao da atual (@schedule ainda nao criada, portando nao precisa de um where.not)
+    # caso o old_schedule tenha uma emergencia ativa, ela precisa receber schedule_id = nil
     old_schedules = Schedule.where(active: true).where(user_id: current_user)
-    old_schedules.each { |old_schedule| old_schedule.update(active: false) }
+    old_schedules.each do |old_schedule|
+      old_schedule.update(active: false)
+      if old_schedule.emergencies.exists?(time_end: nil)
+        old_schedule.emergencies.where(time_end: nil).update_all(schedule_id: nil)
+      end
+    end
 
     if @schedule.save!
       redirect_to @schedule, notice: 'Você está logado.'
