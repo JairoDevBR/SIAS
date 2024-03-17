a74fc970ee9022d1a5322e6fdd48e928cd8adf2b
Keny Ng <kcln88@hotmail.com>
2024-03-17 19:04:22 -0300

    ambulancia ao fazer logout, caso ainda esteja com emergencia ativa, essa emergencia é desvinculada dele

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,14 @@
 class SessionsController < Devise::SessionsController
   def destroy
-    # Altere o atributo active do modelo Schedule para false quando o usuário faz logout
-    current_user.schedules.update_all(active: false)
+    # logica para atribuir active = false para todas as schedules do mesmo user, com excecao da atual
+    # caso o old_schedule tenha uma emergencia ativa, ela precisa receber schedule_id = nil
+    old_schedules = Schedule.where(active: true).where(user_id: current_user)
+    old_schedules.each do |old_schedule|
+      old_schedule.update(active: false)
+      if old_schedule.emergencies.exists?(time_end: nil)
+        old_schedule.emergencies.where(time_end: nil).update_all(schedule_id: nil)
+      end
+    end
     # Faça o logout do usuário
     super
   end
