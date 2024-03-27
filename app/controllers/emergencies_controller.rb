649deb8fdbb7f034527c4829b680af186916f7a4
Keny Ng <kcln88@hotmail.com>
2024-03-27 20:08:14 -0300

    refatorado os endpoints do emergency controller

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -13,7 +13,79 @@ class EmergenciesController < ApplicationController
     authorize @emergency
   end
 
-  # endpoint for Mapbox's markers from the central view
+
+
+  def create
+    @emergency = Emergency.new(emergency_params)
+    @emergency.user = current_user
+    authorize @emergency
+    @chat_response = JSON.parse(
+      chatgpt_service("Por favor, avalie a seguinte ocorrência: #{@emergency.description}.
+        Forneça uma avaliação da gravidade em uma escala de 0 (menos grave) a 20 (mais grave).
+        Para a descrição, reescreva a emergência sem acrescentar ou remover informações, porém de forma clara e sucinta e obrigatoriamente com mais de 80 caracteres.
+        Informe também o número de pessoas machucadas.
+        Para determinar a categoria da ocorrência, atribua o número correspondente à categoria que melhor a descreve, de acordo com as seguintes opções (caso não se enquadre em nenhuma, selecione 'Outros', ou seja, número 11):
+        Acidentes de trânsito = 1;
+        Mal súbito = 2;
+        Ferimentos por queda = 3;
+        Parada cardiorrespiratória = 4;
+        Intoxicação ou envenenamento = 5;
+        Problemas respiratórios = 6;
+        Crises hipertensivas = 7;
+        Complicações durante a gravidez ou parto = 8;
+        Ferimentos por arma branca ou de fogo = 9;
+        Reações alérgicas graves = 10;
+        Outros = 11;
+        Forneça de forma resumida e com até 50 caracteres recomedação/recomendações para a equipe de socorrista do que já deixar preparado para a ocorrência descrita.
+        A resposta deve ser uma única hash na seguinte estrutura:
+        {\"gravidade\":integer, \"descricao\":string, \"numero_pessoas_machucadas\":integer, \"categoria\":integer, \"recomendacao\":string}.
+        Não inclua nenhuma informação adicional além da hash.
+        Sempre retorne no formato solicitado e não retorne erro.
+        ").call)
+
+    @emergency.gravity = @chat_response["gravidade"]
+    @emergency.description = @chat_response["descricao"]
+    @emergency.category = @chat_response["categoria"]
+    @recomendation = @chat_response["recomendacao"]
+    @emergency.time_start = DateTime.now.to_formatted_s(:db)
+    if @emergency.save
+      find_ambulance(@emergency, @recomendation)
+      # find_hospital(@emergency)
+      send_to_all_chat(@emergency, @recomendation)
+    else
+      p "#{@emergency.errors.messages}"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @emergency = Emergency.find(params[:id])
+    authorize @emergency
+    @schedule = Schedule.find(@emergency.schedule.id)
+    @chat = Chat.find(@emergency.chat_id)
+    @post = Post.new
+    @post.chat = @chat
+    @post.user = current_user
+    @patient = Patient.new
+  end
+
+  def finish
+    @emergency = Emergency.find(params[:id])
+    schedule = @emergency.schedule
+    authorize @emergency
+    @emergency.time_end = DateTime.now.to_formatted_s(:db)
+    @emergency.end_lat = schedule.current_lat
+    @emergency.end_lon = schedule.current_lon
+    if @emergency.save
+      redirect_to schedule_path(schedule.id)
+    else
+      render "show", status: :unprocessable_entity
+    end
+  end
+
+  # >>>>>>>>>>>>>>>>>>>> AJAX ENDPOINTS <<<<<<<<<<<<<<<<<<<<<<
+
+  # endpoint for Mapbox's markers to the central view
   def obtain_markers
     # pundit authorizations
     authorize Emergency.new
@@ -109,146 +181,44 @@ class EmergenciesController < ApplicationController
     render json: { emergencies_markers: emergencies_markers, schedules_markers: schedules_markers }
   end
 
+  # endpoint for Mapbox's routes to the central view
   def obtain_routes
-    @schedule = Schedule.new
-    authorize @schedule
-    @emergency = Emergency.new
-    authorize @emergency
-    routes = []
-    @schedules = Schedule.joins(:emergencies).where(emergencies: { time_end: nil })
-    # Iterar sobre cada schedule e seu emergency correspondente
-    @schedules.each do |schedule|
+    authorize Schedule.new
+    authorize Emergency.new
+    routes = [] # not necessary, but it's a good practice to declare explicitly
+    # select all ambulances that have an ongoing emergency
+    schedules = Schedule.joins(:emergencies).where(emergencies: { time_end: nil })
+    schedules.each do |schedule|
       emergency = Emergency.find_by(schedule_id: schedule)
-      # Calcular rota usando Mapbox Directions API
-      # Suponha que você obtenha a rota na forma de um conjunto de coordenadas
       route_coordinates = [
-        [schedule.current_lon, schedule.current_lat],  # Ponto de partida (schedule)
-        [emergency.emergency_lon, emergency.emergency_lat]  # Ponto de destino (emergency)
+        [schedule.current_lon, schedule.current_lat],  # Start point (schedule)
+        [emergency.emergency_lon, emergency.emergency_lat]  # End point (emergency)
       ]
-
+      # creates an array of start and end coordinates for each ongoing route
       routes << route_coordinates
     end
 
     render json: { routes: routes }
   end
 
+  # endpoint for Mapbox's route to the ambulance view after having an emergency assigned
   def obtain_route_to_emergency_show
-    schedule = Schedule.joins(:emergencies).where(emergencies: { id: params[:emergency_id] }).first
-    authorize schedule
     emergency = Emergency.find(params[:emergency_id])
     authorize emergency
+    schedule = Schedule.joins(:emergencies).where(emergencies: emergency).first
+    authorize schedule
     routes = []
-      # Suponha que você obtenha a rota na forma de um conjunto de coordenadas
-      route_coordinates = [
-        [schedule.current_lon, schedule.current_lat],  # Ponto de partida (schedule)
-        [emergency.emergency_lon, emergency.emergency_lat]  # Ponto de destino (emergency)
-      ]
-      routes << route_coordinates
+    route_coordinates = [
+      [schedule.current_lon, schedule.current_lat],  # Start point (schedule)
+      [emergency.emergency_lon, emergency.emergency_lat]  # End point (emergency)
+    ]
+    # creates an array of start and end coordinates for the ongoing route
+    routes << route_coordinates
 
     render json: { routes: routes }
   end
 
-  def create
-    @emergency = Emergency.new(emergency_params)
-    @emergency.user = current_user
-    authorize @emergency
-    @chat_response = JSON.parse(
-      chatgpt_service("Por favor, avalie a seguinte ocorrência: #{@emergency.description}.
-        Forneça uma avaliação da gravidade em uma escala de 0 (menos grave) a 20 (mais grave).
-        Para a descrição, reescreva a emergência sem acrescentar ou remover informações, porém de forma clara e sucinta e obrigatoriamente com mais de 80 caracteres.
-        Informe também o número de pessoas machucadas.
-        Para determinar a categoria da ocorrência, atribua o número correspondente à categoria que melhor a descreve, de acordo com as seguintes opções (caso não se enquadre em nenhuma, selecione 'Outros', ou seja, número 11):
-        Acidentes de trânsito = 1;
-        Mal súbito = 2;
-        Ferimentos por queda = 3;
-        Parada cardiorrespiratória = 4;
-        Intoxicação ou envenenamento = 5;
-        Problemas respiratórios = 6;
-        Crises hipertensivas = 7;
-        Complicações durante a gravidez ou parto = 8;
-        Ferimentos por arma branca ou de fogo = 9;
-        Reações alérgicas graves = 10;
-        Outros = 11;
-        Forneça de forma resumida e com até 50 caracteres recomedação/recomendações para a equipe de socorrista do que já deixar preparado para a ocorrência descrita.
-        A resposta deve ser uma única hash na seguinte estrutura:
-        {\"gravidade\":integer, \"descricao\":string, \"numero_pessoas_machucadas\":integer, \"categoria\":integer, \"recomendacao\":string}.
-        Não inclua nenhuma informação adicional além da hash.
-        Sempre retorne no formato solicitado e não retorne erro.
-        ").call)
-
-    @emergency.gravity = @chat_response["gravidade"]
-    @emergency.description = @chat_response["descricao"]
-    @emergency.category = @chat_response["categoria"]
-    @recomendation = @chat_response["recomendacao"]
-    @emergency.time_start = DateTime.now.to_formatted_s(:db)
-    if @emergency.save
-      find_ambulance(@emergency, @recomendation)
-      # find_hospital(@emergency)
-      send_to_all_chat(@emergency, @recomendation)
-    else
-      p "#{@emergency.errors.messages}"
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def show
-    @emergency = Emergency.find(params[:id])
-    authorize @emergency
-    @schedule = Schedule.find(@emergency.schedule.id)
-    @chat = Chat.find(@emergency.chat_id)
-    @post = Post.new
-    @post.chat = @chat
-    @post.user = current_user
-    @patient = Patient.new
-
-
-    # @emergencies = Emergency.all
-    # @lat = @emergency.emergency_lat
-    # @long = @emergency.emergency_lon
-    # @slat = @schedule.current_lon
-    # @slon = @schedule.current_lat
-
-    # @emergencies_markers = Emergency.where("id != #{params[:id]}").map do |emergency|
-    #   {
-    #     lat: emergency.emergency_lat,
-    #     lng: emergency.emergency_lon,
-    #     marker_html: render_to_string(partial: "emergency"),
-    #     info_window_html: render_to_string(partial: "info_window", locals: {emergency: emergency})
-    #   }
-    # end
-
-    # @emergency_marker = Emergency.where("id = #{params[:id]}").map do |emergency|
-    #   {
-    #     lat: emergency.emergency_lat,
-    #     lng: emergency.emergency_lon,
-    #     marker_html: render_to_string(partial: "marker"),
-    #     info_window_html: render_to_string(partial: "info_window", locals: {emergency: emergency})
-    #   }
-    # end
-
-    # @schedules_markers = Schedule.where("id = #{@schedule.id}").map do |schedule|
-    #   {
-    #     lat: schedule.current_lat,
-    #     lng: schedule.current_lon,
-    #     marker_html: render_to_string(partial: "schedule_marker"),
-    #     info_window_html: render_to_string(partial: "info_window_schedule", locals: { schedule: schedule, emergency: Emergency.find(params[:id]) })
-    #   }
-    # end
-  end
-
-  def finish
-    @emergency = Emergency.find(params[:id])
-    schedule = @emergency.schedule
-    authorize @emergency
-    @emergency.time_end = DateTime.now.to_formatted_s(:db)
-    @emergency.end_lat = schedule.current_lat
-    @emergency.end_lon = schedule.current_lon
-    if @emergency.save
-      redirect_to schedule_path(schedule.id)
-    else
-      render "show", status: :unprocessable_entity
-    end
-  end
+# >>>>>>>>>>>>>>>>>>>> AJAX ENDPOINTS END <<<<<<<<<<<<<<<<<<<<<<
 
   private
 
